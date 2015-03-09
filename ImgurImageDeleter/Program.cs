9f07453ceddbb83421fd1ec9f5e67046fe2d833c
Mr. Trvp <marcovquez1995@gmail.com>
2015-03-08 17:18:11 -0700

    Commented example as well as removed the ShareX reader.

--- ImgurImageDeleter/Program.cs
+++ ImgurImageDeleter/Program.cs
@@ -11,16 +11,20 @@ namespace ImgurImageDeleter
             // Example images to be deleted.
             var images = new[]
             {
-                new Image("rCMxwzr", "lo9h9nZuLVLKc6W"),
+                new Image("rCMxwzr", "lo9h9nZuLVLKc6W")
             };
-
+            
+            // Pass the images array to the DeleteMultiple images.
             var deletionResult = Imgur.DeleteMultiple(images).Result;
 
+            // Loop through all the deletion results and find
+            // list the Id along with the status of the deletion.
             foreach (var result in deletionResult)
                 Console.WriteLine("ImageId: {0}\r\nSuccess: {1}",
                                    result.TargetImage.Id,
                                    result.Success);
 
+            // We're done here. User views console and sees results.
             Console.Write("Press any key to exit this application...");
             Console.Read();
         }
